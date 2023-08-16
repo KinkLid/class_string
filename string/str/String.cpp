5eb3c7dc337e2e589ea29304a36bf53816e7cc0b
KinkLid <48802090+KinkLid@users.noreply.github.com>
2023-08-16 09:57:57 +0300

    разделение
    разделение на два файла

---
+++ string/str/String.cpp
@@ -0,0 +1,126 @@
+#include "String.h"
+
+
+	String::String() = default;
+
+	String::String(const char* const& str) : size_(strlen(str)), cap_(size_ * 2), str_(new char[cap_]) {
+		memcpy(str_, str, cap_);
+	}
+
+	//copy
+	String::String(const String& s) : size_(s.size_), cap_(size_ * 2), str_(new char[cap_]) {
+		memcpy(str_, s.str_, cap_);
+	}
+	//move
+	String::String(String&& s) : size_(s.size_), cap_(size_ * 2), str_(s.str_) {
+		s.size_ = 0;
+		s.cap_ = 1;
+		s.str_ = nullptr;
+	}
+
+	size_t String::size() const { return size_; }
+	size_t String::size() { return size_; }
+
+	//ne fact chto pravilno
+	char* String::begin() { return str_; }
+	char* String::end() { return str_ + size_; }
+
+	bool String::empty() { return size_; }
+
+	String& String::operator=(const String& s) {
+		delete[] str_;
+		size_ = s.size_;
+		cap_ = s.cap_;
+		str_ = new char[cap_];
+		memcpy(str_, s.str_, size_);
+		return *this;
+	}
+
+	String& String::operator=(const char* str) {
+		delete[] str_;
+		size_ = strlen(str);
+		cap_ = size_ * 2;
+		str_ = new char[cap_];
+		memcpy(str_, str, size_);
+		return *this;
+	}
+
+	String& String::operator=(String&& s) {
+		delete[] str_;
+		size_ = s.size_;
+		cap_ = s.cap_;
+		str_ = s.str_;
+		s.size_ = 0;
+		s.cap_ = 1;
+		s.str_ = nullptr;
+		return *this;
+	}
+
+	String& String::operator+=(const String& s) {
+		cap_ = (size_ + s.size_) * 2;
+		char* ss = new char[cap_];
+		memcpy(ss, str_, size_);
+		memcpy(ss + size_ + 1, s.str_, s.size_);
+		delete[] str_;
+		size_ = size_ + s.size_;
+		str_ = ss;
+		return *this;
+	}
+
+	String String::operator+(const String& s) {
+		String ss = *this;
+		ss += s;
+		return ss;
+	}
+
+	String String::operator*(int a) {
+		String s;
+		s.size_ = size_ * a;
+		s.cap_ = s.size_ * 2;
+		s.str_ = new char[s.cap_];
+		for (int i = 0; i < a; ++i) {
+			memcpy(s.str_ + 1 * i, str_, size_);
+		}
+		return s;
+	}
+
+	char& String::operator[](size_t i) const {
+		if (i >= 0 && i <= size_)
+			return str_[i];
+		std::cout << "daleko ti polez, drug";
+		char a = ' ';
+		return a;
+	}
+
+	void String::push_back(const char* c) {
+		if (strlen(c) + size_ >= cap_) {
+			cap_ *= 2;
+			char* str = new char[cap_];
+			memcpy(str, str_, size_);
+			memcpy(str + size_ + 1, c, strlen(c));
+			delete[] str_;
+			str_ = str;
+			size_ += strlen(c);
+		}
+
+	}
+
+
+	String::~String() {
+		delete[] str_;
+	}
+	//friend istream& operator>>(istream& in, String& s);
+
+/*
+ostream& operator<<(ostream& os, const String& s) {
+	for (size_t i = 0; i < s.size(); ++i) {
+		os << s[i];
+	}
+	return os;
+}
+
+//hz rabotaet li
+istream& operator>>(istream& in, String& s) {
+	in >> s;
+	return in;
+}*/

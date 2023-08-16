060bc81f2a1aa7f27802ece975430f49121d5b05
KinkLid <48802090+KinkLid@users.noreply.github.com>
2023-08-16 09:56:25 +0300

    разделение
    разделение а два файла

--- string/str/String.h
+++ string/str/String.h
@@ -10,114 +10,32 @@ class String {
 	
 public:
 	
-	String() = default;
+	String();
 
-	explicit String(const char* const& str) : size_(strlen(str)), cap_(size_ * 2),  str_(new char[cap_]) {
-		memcpy(str_, str, cap_);
-	}
+	explicit String(const char* const&);
+	String(const String&);
+	String(String&&);
 
-	//copy
-	String(const String& s) : size_(s.size_), cap_(size_ * 2), str_(new char[cap_]) {
-		memcpy(str_, s.str_, cap_);
-	}
-	//move
-	String(String&& s) : size_(s.size_), cap_(size_ * 2), str_(s.str_) {
-		s.size_ = 0;
-		s.cap_ = 1;
-		s.str_ = nullptr;
-	}
-
-	size_t size() const { return size_; }
-	size_t size() { return size_; }
+	size_t size() const;
+	size_t size();
 
 	//ne fact chto pravilno
-	char* begin() { return str_; }
-	char* end() { return str_ + size_; }
+	char* begin();
+	char* end();
+	bool empty();
 
-	bool empty() { return size_; }
+	String& operator=(const String&);
+	String& operator=(const char*);
+	String& operator=(String&&);
+	String& operator+=(const String&);
+	String operator+(const String&);
+	String operator*(int);
+	char& operator[](size_t) const;
 
-	String& operator=(const String& s) {
-		delete[] str_;
-		size_ = s.size_;
-		cap_ = s.cap_;
-		str_ = new char[cap_];
-		memcpy(str_, s.str_, size_);
-		return *this;
-	}
-
-	String& operator=(const char* str) {
-		delete[] str_;
-		size_ = strlen(str);
-		cap_ = size_ * 2;
-		str_ = new char[cap_];
-		memcpy(str_, str, size_);
-		return *this;
-	}
-
-	String& operator=(String&& s) {
-		delete[] str_;
-		size_ = s.size_;
-		cap_ = s.cap_;
-		str_ = s.str_;
-		s.size_ = 0;
-		s.cap_ = 1;
-		s.str_ = nullptr;
-		return *this;
-	}
-
-	String& operator+=(const String& s) {
-		cap_ = (size_ + s.size_) * 2;
-		char* ss = new char[cap_];
-		memcpy(ss, str_, size_);
-		memcpy(ss + size_ + 1, s.str_, s.size_);
-		delete[] str_;
-		size_ = size_ + s.size_;
-		str_ = ss;
-		return *this;
-	}
-
-	String operator+(const String& s) {
-		String ss = *this;
-		ss += s;
-		return ss;
-	}
-
-	String operator*(int a) {
-		String s;
-		s.size_ = size_ * a;
-		s.cap_ = s.size_ * 2;
-		s.str_ = new char[s.cap_];
-		for (int i = 0; i < a; ++i) {
-			memcpy(s.str_ + 1 * i, str_, size_);
-		}
-		return s;
-	}
-
-	char& operator[](size_t i) const {
-		if (i >= 0 && i <= size_)
-			return str_[i];
-		std::cout << "daleko ti polez, drug";
-		char a = ' ';
-		return a;
-	}
-
-	void push_back(const char* c) {
-		if (strlen(c) + size_ >= cap_){
-			cap_ *= 2;
-			char* str = new char[cap_];
-			memcpy(str, str_, size_);
-			memcpy(str + size_ + 1, c, strlen(c));
-			delete[] str_;
-			str_ = str;
-			size_ += strlen(c);
-		}
-
-	}
+	void push_back(const char*);
 
 
-	~String() {
-		delete[] str_;
-	}
+	~String();
 	//friend istream& operator>>(istream& in, String& s);
 };
 /*

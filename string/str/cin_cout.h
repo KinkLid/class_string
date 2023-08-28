6297f6d47f3de312090da4dc08bb8007e89a5545
KinkLid <48802090+KinkLid@users.noreply.github.com>
2023-08-28 10:32:06 +0300

    cin fix
    работает, но типо 255 максимум ввести, или сколько хочешь, но факт в том что я хз как сделать это динамическим

--- string/str/cin_cout.h
+++ string/str/cin_cout.h
@@ -8,9 +8,12 @@ std::ostream& operator<<(std::ostream& os, const String& s) {
 	return os;
 }
 
-//ne rabotaet
-std::istream& operator>>(std::istream& in, const char* c) {
+//kostili - razmer 255 char
+std::istream& operator>>(std::istream& in, String& s) {
+	String ss = "";
+	char* c = new char[255];
 	in >> c;
-	String s(c);
+	ss.push_back(c);
+	s = ss;
 	return in;
 }
